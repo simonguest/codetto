3e67ee41909741ea9dd06c87bdf118568c15d276
Simon Guest <me@simonguest.com>
2025-11-02 17:03:27 -0800

    Fixed bug with matplotlib reload

--- src/pyodide/python_reset_globals.py
+++ src/pyodide/python_reset_globals.py
@@ -1,4 +1,4 @@
 # Remove everything except built-ins and tranfomers/overrides
 for name in list(globals().keys()):
-    if not name.startswith('_') and name not in ['__builtins__', '__name__', '__doc__', 'ast', 'InputTransformer', 'js', 'io']:
+    if not name.startswith('_') and name not in ['__builtins__', '__name__', '__doc__', 'ast', 'InputTransformer', 'js', 'io', 'base64']:
         del globals()[name]
