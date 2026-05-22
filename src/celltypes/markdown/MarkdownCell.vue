43b45d707b90b3d7669938eeb4d1d9054beecbd4
Simon Guest <me@simonguest.com>
2026-05-22 17:31:31 +0200

    Fixed spacing issues in markdown cells

--- src/celltypes/markdown/MarkdownCell.vue
+++ src/celltypes/markdown/MarkdownCell.vue
@@ -27,9 +27,43 @@ const processedSource = computed(() => {
 <template>
   <v-card variant="text" max-width="800" class="pt-2 pb-2 ma-auto">
     <v-card-text>
-      <div 
+      <div
+        class="markdown-content"
         v-html="toMarkdown(processedSource)"
       ></div>
     </v-card-text>
   </v-card>
 </template>
+
+<style scoped>
+.markdown-content :deep(h1),
+.markdown-content :deep(h2),
+.markdown-content :deep(h3),
+.markdown-content :deep(h4),
+.markdown-content :deep(h5),
+.markdown-content :deep(h6) {
+  margin-block-start: 1em;
+  margin-block-end: 0.5em;
+}
+
+.markdown-content :deep(h1:first-child),
+.markdown-content :deep(h2:first-child),
+.markdown-content :deep(h3:first-child),
+.markdown-content :deep(h4:first-child),
+.markdown-content :deep(h5:first-child),
+.markdown-content :deep(h6:first-child) {
+  margin-block-start: 0;
+}
+
+.markdown-content :deep(p),
+.markdown-content :deep(ul),
+.markdown-content :deep(ol) {
+  margin-block-end: 1em;
+}
+
+.markdown-content :deep(p:last-child),
+.markdown-content :deep(ul:last-child),
+.markdown-content :deep(ol:last-child) {
+  margin-block-end: 0;
+}
+</style>
